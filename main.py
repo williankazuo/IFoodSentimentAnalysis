38bd80fe6afe14e81c722b0b0f38091d2a8c5030
Willian Kazuo <willian.koga@einstein.br>
2020-07-25 19:22:56 -0300

    Quase finalizado

--- main.py
+++ main.py
@@ -21,27 +21,40 @@ def getrating(driver):
             data['comentarios'].append(comment_user[0].text)
         
 if __name__ == "__main__":
-    url = 'https://www.ifood.com.br/delivery/sao-caetano-do-sul-sp'
+    # url = 'https://www.ifood.com.br/delivery/sao-caetano-do-sul-sp'
+    url = 'https://www.ifood.com.br/lista-restaurantes'
     # page = requests.get(url)
     # soup = BeautifulSoup(page.text, 'html.parser')
     
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(3)
-    list_elements = driver.find_elements_by_class_name('restaurant-card__wrapper')
-    # for elem in list_elements:
-    list_elements[0].click()
-    time.sleep(3)
-    try:
-        modal = driver.find_element_by_class_name('takeout-app-modal__button-ok')
-        modal.click()
-    except:
-        print(sys.exc_info()[0])
 
-    try:
-        btn_rating = driver.find_element_by_class_name('restaurant-rating__button')
-        btn_rating.click()
-        getrating(driver)
-    except:
-        print(sys.exc_info()[0])
-    print('teste')
+    modalUseMyLocation = driver.find_element_by_class_name('btn-address--full-size')
+    modalUseMyLocation.click()
+    time.sleep(3)
+
+    list_elements = driver.find_elements_by_class_name('restaurant-card__wrapper')
+    hrefs = list()
+    for elem in list_elements:
+        hrefs.append(elem.get_attribute('href'))
+    
+    for elem in hrefs:
+        driver.get(elem)
+        time.sleep(3)
+        try:
+            modalTakeOut = driver.find_element_by_class_name('takeout-app-modal__button-ok')
+            modalTakeOut.click()
+
+        except:
+            print(sys.exc_info()[0])
+
+        try:
+            btn_rating = driver.find_element_by_class_name('restaurant-rating__button')
+            btn_rating.click()
+            getrating(driver)
+        except:
+            print(sys.exc_info()[0])
+        driver.execute_script("window.history.go(-1)")
+        time.sleep(3)
+        
