febefc900e49750b8e81ff7006556c4369d243c0
Will <willian.kazuo@hotmail.com>
2020-07-26 16:27:35 -0300

    Arrumar for e utilizar pandas para guardar os dados

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ data = {
     'comentarios': []
 }
 
-def getrating(driver):
+def getRating(driver):
     div_data = driver.find_elements_by_class_name('rating-evaluation__wrapper')
     for evaluation in div_data:
         innerHTML = evaluation.get_attribute('innerHTML')
@@ -19,29 +19,20 @@ def getrating(driver):
             rating_user = soup.findAll('span', {'class': 'rating-evaluation-header__rate'})
             data['notas'].append(rating_user[0].text)
             data['comentarios'].append(comment_user[0].text)
-        
-if __name__ == "__main__":
-    # url = 'https://www.ifood.com.br/delivery/sao-caetano-do-sul-sp'
-    url = 'https://www.ifood.com.br/lista-restaurantes'
-    # page = requests.get(url)
-    # soup = BeautifulSoup(page.text, 'html.parser')
-    
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(3)
 
-    modalUseMyLocation = driver.find_element_by_class_name('btn-address--full-size')
-    modalUseMyLocation.click()
-    time.sleep(3)
 
+def listRestaurants():   
     list_elements = driver.find_elements_by_class_name('restaurant-card__wrapper')
     hrefs = list()
+
+    
     for elem in list_elements:
         hrefs.append(elem.get_attribute('href'))
     
     for elem in hrefs:
         driver.get(elem)
         time.sleep(3)
+
         try:
             modalTakeOut = driver.find_element_by_class_name('takeout-app-modal__button-ok')
             modalTakeOut.click()
@@ -52,9 +43,36 @@ if __name__ == "__main__":
         try:
             btn_rating = driver.find_element_by_class_name('restaurant-rating__button')
             btn_rating.click()
-            getrating(driver)
+            getRating(driver)
         except:
             print(sys.exc_info()[0])
-        driver.execute_script("window.history.go(-1)")
-        time.sleep(3)
+
+    count = len(list_elements)
+
+        
+if __name__ == "__main__":
+    url = 'https://www.ifood.com.br/lista-restaurantes'
+    
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(3)
+
+    modalUseMyLocation = driver.find_element_by_class_name('btn-address--full-size')
+    modalUseMyLocation.click()
+    time.sleep(3)
+
+    countRestaurants = 0
+    loadMore = True
+    while loadMore:
+        listRestaurants(countRestaurants)
+        try:
+            btnLoadMore = driver.find_element_by_class_name('restaurants-list__load-more')
+        except:
+            loadMore = False
+            print(sys.exc_info()[0])
+            
+
+
+    
+    
         
